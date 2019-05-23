bf36f9869b159aa11bb4cbb912f439e2c73467d1
Kevin Chen <kevchen21@gmail.com>
2019-05-22 21:54:21 -0400

    Progress checkin

---
+++ util.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+    Cyclically rotates and array
+'''
+def rotate(collection, rotateAmount):
+    if rotateAmount > 0:
+        shift = rotateAmount % len(collection)
+        collection = (collection[len(collection)-shift:] + collection[:len(collection)-shift])
+        return collection
+    else:
+        shift = (-1*rotateAmount) % len(collection)
+        collection = (collection[shift:] + collection[:shift])
+        return collection
